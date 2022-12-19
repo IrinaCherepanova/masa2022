d2e379bd2adefb4cf28c4a33183743d186dfaa50
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-19 20:31:25 +0300

    rubai

--- first_project/src/HelloWorld.java
+++ first_project/src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println();
-        System.out.println("Hello, world!");
+        System.out.println("         РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                  О. Хайям");
     }
 }
