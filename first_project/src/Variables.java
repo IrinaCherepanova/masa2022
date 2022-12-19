daa7427a41fe67910dad97ea48b52534ddb9ffbf
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-19 22:19:17 +0300

    Create Variables.java

---
+++ first_project/src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        byte myByte;
+        myByte = 4;
+        myByte = 10;
+        byte mybyte = -128;
+        System.out.println(myByte);
+        short myShort = 23123;
+
+        int myInt = 1232444131;
+
+        long myLong = 1231231231232L;
+
+        String myName = "Irina";
+
+    }
+}
