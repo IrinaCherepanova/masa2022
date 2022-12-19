bd4c05dcc25709f1a76f0544dee8130c292e7b03
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-19 19:25:52 +0300

    First commit

--- first_project/src/HelloWorld.java
+++ first_project/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println();
+        System.out.println("Hello, world!");
     }
 }
