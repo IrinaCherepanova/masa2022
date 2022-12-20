7b7fd78520551cb687236ea7ed30b08978476bd5
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-20 21:22:46 +0300

    Create Hometask20122022.java

---
+++ first_project/src/Hometask20122022.java
@@ -0,0 +1,47 @@
+public class Hometask20122022 {
+    public static void main(String[] args) {
+        byte myByte1 = 1;
+        byte myByte2 = 2;
+        System.out.println("myByte1 = " + myByte1);
+        System.out.println("myByte2 = " + myByte2);
+
+        short myShort1 = -32768;
+        short myShort2 = 32767;
+        System.out.println("myShort1 = " + myShort1);
+        System.out.println("myShort2 = " + myShort2);
+
+        int myInt1 = 123123232;
+        int myInt2 = -342342333;
+        System.out.println("myInt1 = " + myInt1);
+        System.out.println("myInt2 = " + myInt2);
+
+        long myLong1 = 23423423423423L;
+        long myLong2 = 34323434343343L;
+        System.out.println("myLong1 = " + myLong1);
+        System.out.println("myLong2 = " + myLong2);
+
+        float myFloat1 = 21312.2323f;
+        float myFloat2 = -21312.2323f;
+        System.out.println("myFloat1 = " + myFloat1);
+        System.out.println("myFloat2 = " + myFloat2);
+
+        double myDouble1 = 342342342343234.343423223d;
+        double myDouble2 = -342342342343234.343423223d;
+        System.out.println("myDouble1 = " + myDouble1);
+        System.out.println("myDouble2 = " + myDouble2);
+
+        boolean myBoolean1 = true;
+        boolean myBoolean2 = false;
+        System.out.println("myBoolean1 = " + myBoolean1);
+        System.out.println("myBoolean2 = " + myBoolean2);
+
+        char myChar1 = '1';
+        char myChar2 = '\u0123';
+        char myChar3 = 123;
+        char myChar4 = 0x1A;
+        System.out.println("myChar1 = " + myChar1);
+        System.out.println("myChar2 = " + myChar2);
+        System.out.println("myChar3 = " + myChar3);
+        System.out.println("myChar4 = " + myChar4);
+    }
+}
