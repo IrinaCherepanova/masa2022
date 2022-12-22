62dfabab1c5d3992977c4cf9f525492b7d659935
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-22 21:57:20 +0300

    Create IncrementDecrement.java

---
+++ first_project/src/Lesson3/IncrementDecrement.java
@@ -0,0 +1,12 @@
+package Lesson3;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("a = " + a);
+        a += 5;
+        System.out.println("a = " + a);
+
+    }
+}
