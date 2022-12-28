4f3f02be146b51e16603110c572c92e84d57df2f
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-28 23:10:18 +0300

    Create Interval.java

---
+++ first_project/src/lesson5/homework/Interval.java
@@ -0,0 +1,17 @@
+package lesson5.homework;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int number = input.nextInt();
+
+        for (int i = 0; i < 100; i++) {
+            if (i==number)
+
+        }
+    }
+}
