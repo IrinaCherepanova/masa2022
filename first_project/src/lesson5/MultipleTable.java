f7acdbc9b77fe6c0abfd1f8475531720a1342969
IrinaCherepanova <cherepanovaira@gmail.com>
2022-12-28 22:57:01 +0300

    Create MultipleTable.java

---
+++ first_project/src/lesson5/MultipleTable.java
@@ -0,0 +1,14 @@
+package lesson5;
+
+public class MultipleTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.printf("%4d", i*j);
+
+            }
+            System.out.println("\n");
+        }
+    }
+}
