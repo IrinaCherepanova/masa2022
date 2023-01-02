b4a4aa10743bffff6c634e44002be1236c1a0dce
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-02 15:20:53 +0300

    Update Interval.java

--- first_project/src/lesson5/homework/Interval.java
+++ first_project/src/lesson5/homework/Interval.java
@@ -6,12 +6,32 @@ public class Interval {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a number:");
-        int number = input.nextInt();
+        int number;
+        String interval = "";
 
-        for (int i = 0; i < 100; i++) {
-            if (i==number)
-
+        do{
+            System.out.println("Please enter a number from 0 to 100:");
+            number = input.nextInt();
+            if (number<0 || number > 100){
+                System.out.println("Wrong number!");
+            }
         }
+        while (number<0 || number > 100);
+
+        if (number>=0 && number<=14){
+            interval = "[0 - 14]";
+        }
+        else if (number>=15 && number<=35){
+            interval = "[15 - 35]";
+        }
+        else if (number>=36 && number<=50){
+            interval = "[36 - 50]";
+        }
+        else if (number>=50 && number<=100){
+            interval = "[50 - 100]";
+        }
+
+        System.out.println(number + " is in the interval " + interval);
+        input.close();
     }
 }
