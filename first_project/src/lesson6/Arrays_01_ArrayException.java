5be7a9277bb04c67772e3e6da1482eb43fdabae5
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-02 21:37:50 +0300

    new arrays

---
+++ first_project/src/lesson6/Arrays_01_ArrayException.java
@@ -0,0 +1,9 @@
+package lesson6;
+
+public class Arrays_01_ArrayException {
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 2, 3};
+        array1[4] = 12;
+    }
+}
---
+++ first_project/src/lesson6/Arrays_02_ForEachLoop.java
@@ -0,0 +1,21 @@
+package lesson6;
+
+public class Arrays_02_ForEachLoop {
+    public static void main(String[] args) {
+
+        int arrayOfNumbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < arrayOfNumbers.length; i++) {
+            System.out.println(arrayOfNumbers[i] + " ");
+
+        }
+
+        for (int number : arrayOfNumbers){
+            System.out.println(number + " ");
+        }
+
+        for (int number : arrayOfNumbers) {
+            System.out.println(number + " ");
+        }
+    }
+}
