4b80909d2c55433c68b55d31deaf1ce73ad11ede
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-02 19:15:12 +0300

    Create RandomValues.java

---
+++ first_project/src/lesson6/RandomValues.java
@@ -0,0 +1,26 @@
+package lesson6;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomValues {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        ThreadLocalRandom treadLocalRandom = ThreadLocalRandom.current();
+
+//        int randomInteger = random.nextInt();
+//        int randomIntegerWithBound = random.nextInt(10);
+//        double randomDouble = random.nextDouble();
+
+//        System.out.println("randomInteger = " + randomInteger);
+//        System.out.println("randomIntegerWithBound = " + randomIntegerWithBound);
+//        System.out.println("randomDouble = " + randomDouble);
+
+        int randomIntegerWithBounds = treadLocalRandom.nextInt(10, 20);
+        System.out.println("randomIntegerWithBounds = " + randomIntegerWithBounds);
+
+        
+
+    }
+}
