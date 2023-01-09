af92242a187a5ce3cef95b7bd87e6e81e3f1493a
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-09 20:57:59 +0300

    Create Methods_04_ReturnWithParams.java

---
+++ first_project/src/lesson7/methods/Methods_04_ReturnWithParams.java
@@ -0,0 +1,12 @@
+package lesson7.methods;
+
+public class Methods_04_ReturnWithParams {
+    public static void main(String[] args) {
+
+        System.out.println("sumNumbers(1,2) = " + sumNumbers(1, 2));
+    }
+
+    public static int sumNumbers(int a, int b){
+        return a + b;
+    }
+}
