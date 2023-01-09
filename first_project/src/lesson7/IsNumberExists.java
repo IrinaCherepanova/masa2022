5d8ec43ff27d06b8e67ee0b7787d77f71d58c636
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-09 22:15:43 +0300

    added checkNumberInArrayByStream

--- first_project/src/lesson7/IsNumberExists.java
+++ first_project/src/lesson7/IsNumberExists.java
@@ -2,6 +2,7 @@ package lesson7;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class IsNumberExists {
     public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class IsNumberExists {
         int myNumber = input.nextInt();
 
         System.out.println("checkNumberInArray(myNumber, myArray) = " + checkNumberInArray(myNumber, myArray));
+        System.out.println("checkNumberInArrayByStream(myNumber, myArray) = " + checkNumberInArrayByStream(myNumber, myArray));
     }
 
     public static boolean checkNumberInArray(int myValue, int[] array){
@@ -24,6 +26,10 @@ public class IsNumberExists {
 
         return false;
 
+    }
 
+    public static boolean checkNumberInArrayByStream(int myValue, int[] array){
+        int findedValue = IntStream.range(0, array.length).filter(i -> myValue == array[i]).findFirst().orElse(-1);
+        return findedValue!=-1? true: false;
     }
 }
