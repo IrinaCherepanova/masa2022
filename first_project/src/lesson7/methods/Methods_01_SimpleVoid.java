1e7412e6c498a58868ba8d2173a30350bafa9863
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-09 20:04:09 +0300

    Create Methods_01_SimpleVoid.java

---
+++ first_project/src/lesson7/methods/Methods_01_SimpleVoid.java
@@ -0,0 +1,18 @@
+package lesson7.methods;
+
+import java.util.Scanner;
+
+public class Methods_01_SimpleVoid {
+    public static void main(String[] args) {
+
+        sayHelloToClass();
+
+    }
+
+    public static void sayHelloToClass(){
+        System.out.println("Hello, Alex!");
+        System.out.println("Hello, Andrei!");
+        System.out.println("Hello, Irina!");
+    }
+
+}
