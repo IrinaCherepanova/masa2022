fe0b3a3cccd989d26949a061e7c9f55479e3160b
IrinaCherepanova <cherepanovaira@gmail.com>
2023-01-12 23:01:56 +0300

    cats

---
+++ first_project/src/lesson8/cats/Cat.java
@@ -0,0 +1,46 @@
+package lesson8.cats;
+
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ first_project/src/lesson8/cats/MCats.java
@@ -0,0 +1,18 @@
+package lesson8.cats;
+
+import java.util.ArrayList;
+
+public class MCats {
+    public static void main(String[] args) {
+
+        ArrayList<Cat> cats = new ArrayList<>();
+        cats.add(new Cat("Bulochka", "Grey", 3));
+        cats.add(new Cat("Pirozhok", "White", 2));
+        cats.add(new Cat("Bublic", "Grey", 1));
+        cats.add(new Cat("Ponchik", "Black", 3));
+        cats.add(new Cat("Baranka", "White", 4));
+
+        cats.forEach(cat -> System.out.println(cat.toString()));
+        
+    }
+}
